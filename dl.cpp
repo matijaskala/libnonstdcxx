ac19af5f2cbc911646983d5773e68c999b8bed51
Matija Skala <mskala@gmx.com>
2016-11-10 11:30:27 +0100

    fix namespace dl

--- dl.cpp
+++ dl.cpp
@@ -46,7 +46,7 @@ bool dl::close ( void* handle ) {
 #endif
 }
 
-void* sym ( void* handle, char* name ) {
+void* dl::sym ( void* handle, char* name ) {
 #ifdef _WIN32
     FARPROC sym = GetProcAddress ( static_cast<HMODULE> ( handle ), name );
     return reinterpret_cast<void*> ( sym );
